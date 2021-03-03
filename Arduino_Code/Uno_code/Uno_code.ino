52b0e111abd9fd83e663553cf1c632c427dc1791
JosManoel <josmanoelfreitas2012@gmail.com>
2021-03-03 14:06:49 -0300

    ADD code Arduino

---
+++ Arduino_Code/Uno_code/Uno_code.ino
@@ -0,0 +1,18 @@
+/******************************************************\
+ * Arduino Shield Sterring Wheel - CODE ARDUINO UNO   *
+ *                                                    *
+ * By: JosManoel                                      *
+ * E-mail: josmanoelfreitas2012@gmail.com             *
+ * GitHub: https://github.com/JosManoel               *
+\******************************************************/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
