a69cc1535908289f2b909939ea56b3454ed7f28e
JosManoel <josmanoelfreitas2012@gmail.com>
2021-03-21 17:59:04 -0300

    Add Uno_code rev 1.1

--- Arduino_Code/Uno_code/Uno_code.ino
+++ Arduino_Code/Uno_code/Uno_code.ino
@@ -1,101 +1,192 @@
-/******************************************************\
- * Arduino Shield Sterring Wheel - CODE ARDUINO UNO   *
- *                                                    *
- * By: JosManoel                                      *
- * E-mail: josmanoelfreitas2012@gmail.com             *
- * GitHub: https://github.com/JosManoel               *
-\******************************************************/
+/********************************************************************\
+ *        Arduino Shield Sterring Wheel - CODE ARDUINO UNO          *
+ *                                                                  *
+ * By: JosManoel                                                    *
+ * E-mail: josmanoelfreitas2012@gmail.com                           *
+ * GitHub: https://github.com/JosManoel                             *
+\********************************************************************/
 
-////////////////////////////////////////////////////////
-////                     CONEXOES                   ////
-////////////////////////////////////////////////////////
-
-#define pulse_val 5   // definindo tempo de deslocamento do clock e load
-
-////////////////////////////////////////////////////////
-////                     CONEXOES                   ////
-////////////////////////////////////////////////////////
-
-// Definindo Pinos de conexões do 74HC165
-int ClockEnb = 10; // CE pin 15
-int DataIn = 11;   // Q7 pin 7 (verificar se pode ser o pin 9 )
-int ClockIn = 12;  // CP pin 2
-int LoadIn = 13;   // PL pin 1
-
-// Definindo Pinos de botoes 
-int PShiftP = 9;   // PShift plus  (cambio borboleta)
-int PShiftM = 8;   // PShift minus (cambio borboleta)
-int Horn = 7;      // Horn Button  (buzina)
-
-// Definindo Entradas analogicas
-int Accel = 0;     // Acelerador
-int Break = 1;     // Freio
-int Clut = 2;      // Embreagem
-int HBreak = 3;    // Freio de mão
-int Strg = 4;      // Volante
-
-// Botoes opcionais
 /*
-int OpAnalog = 5;  // Saída analogica opcional
-int OpButt1 = 2;   // Button opcional 1
-int OpButt2 = 3;   // Button opcional 2
-int OpButt3 = 4;   // Button opcional 3
-int OpButt4 = 5;   // Button opcional 4
-int OpButt5 = 6;   // Button opcional 5
+==============================   NOTA:   =============================
+  
+         Este código foi testado apenas em ambiente simulado, 
+            utilizando o software Proteus na versão 8.1.
+
+  Este código foi desenvolvindo para ser utilizando em conjunto com 
+    o CI 74HC165, um registrador de deslocamento com uma entrada 
+               paralela de 8 bits e uma saída serial. 
+  Este CI foi utilizado para aumentar o numero de portas do Arduino 
+     Uno, possibilitando a construção de um cambio com 8 marchas.
+
+  Acesse o repositório da biblioteca para obter mais informações:
+  GitHub: https://github.com/AlanChatham/UnoJoy
+
+======================================================================
 */
 
-//Pulso de clock no pino Load 
-void pulse_pin(int pulse_clock){
+// Incluindo biblioteca UnoJoy
+#include "UnoJoy.h"
 
+/********************************************************************\
+ *                        VARIAVEIS DE CONTROLE                     *
+\********************************************************************/
+
+#define pulse_val 5   // definindo tempo de deslocamento de load
+byte OldValues;       // variavel de constrole de terminal
+
+/********************************************************************\
+ *                 DEFININDO CONEXÕES COM O ARDUINO                 *
+\********************************************************************/
+
+// LED de info
+const int led = 13;      // LED de info 
+
+// Definindo Pinos de conexões do 74HC165
+const int ClockIn = 12;  // CP pin 2
+const int DataIn = 11;   // Q7 pin 7
+const int ClockEnb = 10; // CE pin 15
+const int LoadIn = 9;    // PL pin 1
+
+// Definindo Pinos de botoes 
+const int PShiftP = 8;   // PShift +
+const int PShiftM = 7;   // PShift -
+const int Home = 6;      // Home PS3
+const int Horn = A5;     // Buzina 
+const int HBrake = A4;   // Freio de mão
+
+// Dpad - Direcionais do joystick
+const int DpadUp = 5;      
+const int DpadDown = 4; 
+const int DpadLeft = 3;
+const int DpadRight = 2;
+
+// Definindo Entradas analogicas
+const int Accel = A0;    // Acelerador
+const int Brake = A1;    // Freio
+const int Clut = A2;     // Embreagem
+const int Strg = A3;     // Volante
+
+/********************************************************************\
+ *                             FUNÇÕES                              *
+\********************************************************************/
+
+void setupPins(){
+  // Setando pinos de conexões do CI 74HC165 
+  pinMode(ClockEnb, OUTPUT);
+  pinMode(DataIn, INPUT);
+  pinMode(ClockIn, OUTPUT);
+  pinMode(LoadIn, OUTPUT);
+
+  // Setando pinos de conexões do botões
+  pinMode(PShiftP, INPUT_PULLUP);
+  pinMode(PShiftM, INPUT_PULLUP);
+  pinMode(HBrake, INPUT_PULLUP);
+  pinMode(Horn, INPUT_PULLUP);
+  pinMode(Home, INPUT_PULLUP);
+
+  // Setando pinos do Dpad
+  pinMode(DpadUp, INPUT_PULLUP);
+  pinMode(DpadDown, INPUT_PULLUP);
+  pinMode(DpadLeft, INPUT_PULLUP);
+  pinMode(DpadRight, INPUT_PULLUP);
+}
+
+void pulsePin(int pulse_clock){
+  // Gera um pulso de clock no pino Load [CI 74HC165]
   digitalWrite(LoadIn, LOW);
   delayMicroseconds(pulse_clock);
   digitalWrite(LoadIn,HIGH);
   delayMicroseconds(pulse_clock);
 }
 
-//Pegando valores e realizando o shiftIn
-byte get_data() {
+byte getGear() {
+  // Recuperando os dados do ci 74HC165 e realizando o shiftIn
   digitalWrite(ClockIn, HIGH);
   digitalWrite(ClockEnb, LOW);
 
-  byte data = shiftIn(DataIn, ClockIn, LSBFIRST);
+  byte data = shiftIn(DataIn, ClockIn, MSBFIRST);
 
   digitalWrite(ClockIn, HIGH);
-
+  // Retornando leitura dos 8 pinos do 74HC165 (1 byte)
   return(data);
 }
 
-//Printando informações no terminal
-void print_data(byte data_info){
+void ledInfo(int rpt){
+  // Aciona o LED de informações
+  for (int l = 0; l < rpt ; l++){ 
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(250);                       
+    digitalWrite(LED_BUILTIN, LOW);   
+    delay(250);                       
+  }
+}
 
-  Serial.print("Estado dos pinos do 74hc165 \n");
-  Serial.println(data_info, BIN);
+dataForController_t getControllerData(byte data_gear){
+
+  /*
+    Limpando dados do buffer com a função getBlankDataForController()
+    presente na biblioteca UnoJoy.
+  */
+  dataForController_t controllerData = getBlankDataForController(); 
+
+  // Setando entradas do cambio
+  /*
+     A leitura das entradas do cambio é realizada na função getGear(),
+     que retorna um byte, onde cada bit simboliza uma marcha a partir 
+     do primeiro bit. O bitRead realiza a leitura de determinado bit 
+     no byte.
+  */
+  controllerData.triangleOn = bitRead(data_gear, 0); // 1° Marcha
+  controllerData.circleOn = bitRead(data_gear, 1);   // 2° Marcha
+  controllerData.squareOn = bitRead(data_gear, 2);   // 3° Marcha
+  controllerData.crossOn = bitRead(data_gear, 3);    // 4° Marcha
+  controllerData.l1On = bitRead(data_gear, 4);       // 5° Marcha
+  controllerData.l2On = bitRead(data_gear, 5);       // 6° Marcha
+  controllerData.l3On = bitRead(data_gear, 6);       // 7° Marcha
+  controllerData.r1On = bitRead(data_gear, 7);       // Marcha Ré
+
+  // Setando paddle shift e outros butões
+  controllerData.r2On = !digitalRead(PShiftP);       // Paddle Shift +
+  controllerData.r3On = !digitalRead(PShiftM);       // Paddle Shift -
+  controllerData.selectOn = !digitalRead(Horn);      // Buzina
+  controllerData.startOn = !digitalRead(HBrake);     // Freio de mão
+  controllerData.homeOn = !digitalRead(Home);        // Home
+  
+  // Setando Dpad
+  controllerData.dpadUpOn = !digitalRead(DpadUp);
+  controllerData.dpadDownOn = !digitalRead(DpadDown);
+  controllerData.dpadLeftOn = !digitalRead(DpadLeft);
+  controllerData.dpadRightOn = !digitalRead(DpadRight);
+
+  // Setando entradas analógicas 
+  /* 
+    Usar o analogRead para realizar a leitura de portas analogias
+    retorna um dado de 10bits de resolução, sendo necessário 
+    realizar o deslocamento de 2 bits para que ela possa ser 
+    utilizada pela biblioteca UnoJoy.
+  */
+  controllerData.leftStickX = analogRead(Strg) >> 2;   // Volante
+  controllerData.leftStickY = analogRead(Accel) >> 2;  // Acelerador
+  controllerData.rightStickX = analogRead(Clut) >> 2;  // Embreagem
+  controllerData.rightStickY = analogRead(Brake) >> 2; // Freio
 
 }
 
 void setup() {
-  
-  // iniciando a comunicação por terminal
-  Serial.begin(9600); 
+  // Setando pinos
+  setupPins();
+  // Inicializando joystick
+  setupUnoJoy();
 
-  //Setando os pinos do arduino
-
-    //Setup 74HC165 connections
-  pinMode(ClockEnb, OUTPUT);
-  pinMode(DataIn, INPUT);
-  pinMode(ClockIn, OUTPUT);
-  pinMode(LoadIn, OUTPUT);
-
-    //Setando pinos de botoes do volante
-  pinMode(PShiftP, INPUT_PULLUP);
-  pinMode(PShiftM, INPUT_PULLUP);
-  pinMode(Horn, INPUT_PULLUP);
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  ledInfo(2);
 }
-
 
 void loop() {
   //aplicando um pulso de clock no load pin
-  pulse_pin(pulse_val);
-  print_data(get_data());
+  pulsePin(pulse_val);
+
+  // Realizando a atualização da letura dos dados
+  dataForController_t controllerData = getControllerData(getGear());
+  setControllerData(controllerData);
 }
