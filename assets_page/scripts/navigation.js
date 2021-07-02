198341c5e86b50a85357caa9dccac519117d5b72
JosManoel <josmanoelfreitas2012@gmail.com>
2021-07-01 21:09:54 -0300

    fixed bug

--- assets_page/scripts/navigation.js
+++ assets_page/scripts/navigation.js
@@ -1,13 +1,13 @@
 function toHome(){
-    parent.window.location.href = "/index.html";
+    parent.window.location.href = "index.html";
 }
 
 function toInfo(){
-    parent.window.location.href = "/documentation/documentation_page.html";
+    parent.window.location.href = "documentation/documentation_page.html";
 }
 
 function toDownloads(){
-    parent.window.location.href = "/downloads/downloads.html";
+    parent.window.location.href = "downloads/downloads.html";
 }
 
 function openRepository(){
